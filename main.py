9efe63f4a1731829542581710e88b309a868202d
n0mori <nick33omori@gmail.com>
2020-09-22 23:55:38 -0300

    delete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 import json
 
 class RazerService:
@@ -12,6 +13,13 @@ class RazerService:
     self.sessionid = body['sessionid']
     self.uri = body['uri']
 
+  def __del__(self):
+    time.sleep(5)
+    print(requests.delete(url=self.uri))
+
+  def send_layout(self, layout):
+    pass
 
 rs = RazerService()
 print(rs.sessionid, rs.uri)
+del rs
